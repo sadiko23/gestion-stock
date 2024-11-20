de81ad890c23955e269156589ce6e1b2ccb0ac7d
pc <fahlaouimohammed@gmail.com>
2024-11-20 21:18:43 +0100

    all2

--- src/main/java/org/example/gestionstock/controllers/CommandeController.java
+++ src/main/java/org/example/gestionstock/controllers/CommandeController.java
@@ -44,6 +44,9 @@ public class CommandeController {
 
 
     }
+
+
+
 //    @GetMapping("/test")
 //    public String test(){
 //        ProduitFinis produitFinis=new ProduitFinis();
--- src/main/java/org/example/gestionstock/controllers/FournisseurController.java
+++ src/main/java/org/example/gestionstock/controllers/FournisseurController.java
@@ -3,6 +3,7 @@ package org.example.gestionstock.controllers;
 import org.example.gestionstock.entity.Fournisseur;
 import org.example.gestionstock.service.FournisseurService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class FournisseurController {
     @Autowired
     private FournisseurService fournisseurService;
     @GetMapping("fournisseur")
+    @CrossOrigin("*")
     public List<Fournisseur> getAllFournisseur() {
         return fournisseurService.findAll();
     }
